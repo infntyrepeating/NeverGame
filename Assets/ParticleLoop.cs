d4cb951f81b482f91a54af32d0a5647f54df45a6
infntyrepeating <infntyrepeating@gmail.com>
2024-01-17 18:54:19 +0000

    Added some Particles

---
+++ Assets/ParticleLoop.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleLoop : MonoBehaviour
+{
+    
+    public float rotationSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
